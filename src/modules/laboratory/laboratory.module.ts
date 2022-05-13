d3015c7372a1fdb296c4463091c1d5b84c30be54
Luis Enrique Ramirez Lopez <luis.ramirez@digitas.com>
2022-05-12 23:23:25 -0500

    get studies of laboratory

--- src/modules/laboratory/laboratory.module.ts
+++ src/modules/laboratory/laboratory.module.ts
@@ -3,17 +3,11 @@ import { LaboratoryService } from './services/laboratory.service';
 import { LaboratoryController } from './controllers/laboratory.controller';
 import { DatabaseModule } from '../database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PassportModule } from '@nestjs/passport';
 import { CatalogoEstudiosEntity } from 'src/entities/catalogo_estudios.entity';
-import { JwtStrategy } from 'src/utils/strategies/jwt.strategy';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    TypeOrmModule.forFeature([CatalogoEstudiosEntity]),
-    PassportModule,
-  ],
-  providers: [JwtStrategy, LaboratoryService],
+  imports: [DatabaseModule, TypeOrmModule.forFeature([CatalogoEstudiosEntity])],
+  providers: [LaboratoryService],
   controllers: [LaboratoryController],
 })
 export class LaboratoryModule {}
